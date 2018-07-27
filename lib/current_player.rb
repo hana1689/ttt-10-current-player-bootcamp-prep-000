e8edfe56c0b16701834d6ce0f15751d1e71584a6
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-27 18:12:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,6 @@
 def turn_count(boards)
   counter = 0
   boards.each do |board|
-    print(board[counter])
     if(board[counter] != " " || board[counter] != "" || board[counter] != nil)
       counter += 1
     end
